3452b5ef57bb783b19a66aff7cd50c7662e97e9f
Ethan <esms4@gmx.com>
2022-11-15 20:36:34 -0500

    added astronaut and planet images

--- main.py
+++ main.py
@@ -12,8 +12,16 @@ import math
 
 ###############################################################################
 def appStarted(app):
+    # image from https://www.pinterest.com/pin/258886678571521581/
     app.background  = app.loadImage("background_image.jpg")
-    app.background = app.scaleImage(app.background, 7/5)
+    app.background = app.scaleImage(app.background, 3/4)
+    # image from https://www.vectorstock.com/royalty-free-vector/
+    # full-moon-cartoon-vector-4118531
+    app.moon = app.loadImage("moon_image.png")
+    app.moon = app.scaleImage(app.moon, 1/3)
+    # image from
+    app.astronaut = app.loadImage("astronaut_image.png")
+    app.astronaut = app.scaleImage(app.astronaut, 1/5)
 
 def keyPressed(app, event):
     pass
@@ -25,7 +33,12 @@ def timerFired(app):
     pass
 
 def redrawAll(app, canvas):
-    canvas.create_image(0, 0, image = ImageTk.PhotoImage(app.background))
+    canvas.create_image(app.width//2, app.height//2,
+                        image = ImageTk.PhotoImage(app.background))
+    canvas.create_image(app.width//2, app.height//2,
+                        image = ImageTk.PhotoImage(app.moon))
+    canvas.create_image(app.width//2, app.height//2,
+                        image = ImageTk.PhotoImage(app.astronaut))
 
 runApp(width = 500, height = 800)
 
